72127fa55c823287d6ad4803f605d78267a70799
mfa077 <miftahf77@gmail.com>
2018-10-08 16:31:10 +0700

    HMI Lakban using PyQt5

--- pyqt5/main.py
+++ pyqt5/main.py
@@ -15,15 +15,52 @@ import serial
 import time
 import threading
 
+# for get data from server using JWT (Json Web Token)
+import json
+import requests
+
 # This is our window from QtCreator
 import mainwindow
 import rework
 
+# Variable for getting data from PC Server
+dataFactoriesURL = 'http://192.168.10.151:8080/api/factories'
+loginURL='http://192.168.10.151:8080/api/auth/login'
+headers = {'content-type': 'application/json'}
+payload = {
+    'email': 'admin@yahoo.com',
+    'password': '123qwe'
+    }
+
+# Get Token (JWT) using post method
+r = requests.post(loginURL,
+                  data=json.dumps(payload),
+                  headers=headers)
+token=r.json()['access_token']
+token_type=r.json()['token_type']
+
+# Get data from API using get method and token
+hed = {'Authorization':token_type+' '+token}
+PARAMS_GET = {'id' : '1'}
+r = requests.get(url = dataFactoriesURL, params = PARAMS_GET, headers=hed)
+# Convert from single quote mark to double quote mark
+json_data=json.dumps(r.json()[0])
+# Parsing json data
+parsed_json=json.loads(json_data)
+# Array of parsed json
+id_=str(parsed_json['id'])
+email=str(parsed_json['email'])
+code=str(parsed_json['code'])
+name=str(parsed_json['name'])
+phone=str(parsed_json['phone'])
+
 # create class for our Raspberry Pi GUI
 class Rework(QMainWindow, rework.Ui_Form):
     def __init__(self):
+        global email
         super(self.__class__, self).__init__()
         self.setupUi(self) # gets defined in the UI file
+        self.line_val.setText(name)
 
 # create class for our Raspberry Pi GUI
 class MainWindow(QMainWindow, mainwindow.Ui_MainWindow):
