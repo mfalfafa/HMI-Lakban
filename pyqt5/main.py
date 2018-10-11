5fd0e066d4106619341905bf8fc1fc07454433a0
mfa077 <miftahf77@gmail.com>
2018-10-11 14:20:22 +0700

    HMI lakban using Qt5

--- pyqt5/main.py
+++ pyqt5/main.py
@@ -2,7 +2,7 @@
 import sys
  
 # For PostgreSQL database 
-import psycopg2
+# import psycopg2
 
 # This gets the Qt stuff
 import PyQt5
@@ -15,77 +15,194 @@ import serial
 import time
 import threading
 
-# for get data from server using JWT (Json Web Token)
-import json
-import requests
-
 # This is our window from QtCreator
 import mainwindow
 import rework
 
-# Variable for getting data from PC Server
-dataFactoriesURL = 'http://192.168.10.151:8080/api/factories'
-loginURL='http://192.168.10.151:8080/api/auth/login'
-headers = {'content-type': 'application/json'}
-payload = {
-    'email': 'admin@yahoo.com',
-    'password': '123qwe'
-    }
+# Windows
+mainwin=''
+reworkwin=''
 
-# Get Token (JWT) using post method
-r = requests.post(loginURL,
-                  data=json.dumps(payload),
-                  headers=headers)
-token=r.json()['access_token']
-token_type=r.json()['token_type']
+# variables
+style_on=("font: 75 17pt \"Arial\";\n"+"color: rgb(0, 0, 255);\n"+"background-color: rgb(180, 255, 255);\n"+"border-color: rgb(0, 0, 255);")
+style_off=("color: rgb(255, 0, 0);\n"+"font: 75 17pt \"Arial\";\n"+"background-color: rgb(214, 214, 214);")
+val_line_active=1
+val_of_line=0
+ready_setup=0
+current_time=0
 
-# Get data from API using get method and token
-hed = {'Authorization':token_type+' '+token}
-PARAMS_GET = {'id' : '1'}
-r = requests.get(url = dataFactoriesURL, params = PARAMS_GET, headers=hed)
-# Convert from single quote mark to double quote mark
-json_data=json.dumps(r.json()[0])
-# Parsing json data
-parsed_json=json.loads(json_data)
-# Array of parsed json
-id_=str(parsed_json['id'])
-email=str(parsed_json['email'])
-code=str(parsed_json['code'])
-name=str(parsed_json['name'])
-phone=str(parsed_json['phone'])
+# components
+line_val=''
+rework_val=''
+val_line23=''
+val_line24=''
+val_line25=''
+val_line26=''
+time_lbl=''
+
+# Python threads
+valueThread=''
+
+class ValueThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        addValue()
+
+def addValue():
+    global current_time,time_lbl
+    global val_line_active,val_line23,val_line24,val_line25,val_line26,val_of_line,ready_setup
+    while 1:
+        if ready_setup ==1:
+            val_of_line=val_of_line+1
+            if val_line_active == 1:
+                val_line23.setText(str(val_of_line))
+            elif val_line_active == 2:
+                val_line24.setText(str(val_of_line))
+            elif val_line_active == 3:
+                val_line25.setText(str(val_of_line))
+            elif val_line_active == 4:
+                val_line26.setText(str(val_of_line))
+            current_time=time.localtime( time.time())
+            time_now=[0]*3
+            time_now[0]=str(current_time.tm_hour)
+            time_now[1]=str(current_time.tm_min)
+            time_now[2]=str(current_time.tm_sec)
+            current_time=''
+            for i in range(3):
+                if len(time_now[i])==1:
+                    time_now[i]='0'+time_now[i]
+            current_time=time_now[0]+':'+time_now[1]+':'+time_now[2]
+            time_lbl.setText(current_time)
+            time.sleep(1)
 
 # create class for our Raspberry Pi GUI
 class Rework(QMainWindow, rework.Ui_Form):
+    def decrement_val(self):
+        global rework_val
+        # Decrement rework values
+        val=int(rework_val.text())
+        if  val > 0:  
+            val =  val-1
+            rework_val.setText(str(val))
+
+    def increment_val(self):
+        global rework_val
+        # Increment Rework val
+        val=int(rework_val.text())
+        if  val < 1000:  
+            val =  val+1
+            rework_val.setText(str(val))
+
+    def submit_rework(self):
+        global rework_val
+        val=rework_val.text()
+        self.close()
+
     def __init__(self):
-        global email
+        global line_val,rework_val
         super(self.__class__, self).__init__()
         self.setupUi(self) # gets defined in the UI file
-        self.line_val.setText(name)
+        # Move to the center of window
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+        # Always on top
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        # Buttons
+        self.pb_prev.clicked.connect(self.decrement_val)
+        self.pb_next.clicked.connect(self.increment_val)
+        self.exit_pb.clicked.connect(self.close)
+        self.pb_submit.clicked.connect(self.submit_rework)
+        line_val=self.line_val
+        rework_val=self.rework_val
+        rework_val.setText('0')
+
 
 # create class for our Raspberry Pi GUI
 class MainWindow(QMainWindow, mainwindow.Ui_MainWindow):
     def pb_rework_press(self):
-        pass
-        
+        global reworkwin
+        reworkwin.show()
+
+    def pb_line23_pressed(self):
+        global val_line23,val_line24,val_line25,val_line26,val_line_active
+        val_line23.setStyleSheet(style_on)
+        val_line24.setStyleSheet(style_off)
+        val_line25.setStyleSheet(style_off)
+        val_line26.setStyleSheet(style_off)
+        val_line_active=1
+    
+    def pb_line24_pressed(self):
+        global val_line23,val_line24,val_line25,val_line26,val_line_active
+        val_line23.setStyleSheet(style_off)
+        val_line24.setStyleSheet(style_on)
+        val_line25.setStyleSheet(style_off)
+        val_line26.setStyleSheet(style_off)
+        val_line_active=2
+
+    def pb_line25_pressed(self):
+        global val_line23,val_line24,val_line25,val_line26,val_line_active
+        val_line23.setStyleSheet(style_off)
+        val_line24.setStyleSheet(style_off)
+        val_line25.setStyleSheet(style_on)
+        val_line26.setStyleSheet(style_off)
+        val_line_active=3
+
+    def pb_line26_pressed(self):
+        global val_line23,val_line24,val_line25,val_line26,val_line_active
+        val_line23.setStyleSheet(style_off)
+        val_line24.setStyleSheet(style_off)
+        val_line25.setStyleSheet(style_off)
+        val_line26.setStyleSheet(style_on)
+        val_line_active=4
+
     def __init__(self):
+        global time_lbl
+        global val_line23,val_line24,val_line25,val_line26,ready_setup
         super(self.__class__, self).__init__()
         self.setupUi(self) # gets defined in the UI file
-        self.val_line23.setMargin(20)
-        self.val_line24.setMargin(20)
-        self.val_line25.setMargin(20)
-        self.val_line26.setMargin(20)
+        val_line23=self.val_line23
+        val_line24=self.val_line24
+        val_line25=self.val_line25
+        val_line26=self.val_line26
+        val_line23.setMargin(20)
+        val_line24.setMargin(20)
+        val_line25.setMargin(20)
+        val_line26.setMargin(20)
+        # Initialization of line active on goods detected
+        val_line23.setStyleSheet(style_on)
         self.pb_rework.released.connect(self.pb_rework_press)
+        self.pb_line23.clicked.connect(self.pb_line23_pressed)
+        self.pb_line24.clicked.connect(self.pb_line24_pressed)
+        self.pb_line25.clicked.connect(self.pb_line25_pressed)
+        self.pb_line26.clicked.connect(self.pb_line26_pressed)
+        # label
+        time_lbl=self.time_lbl
+        # Ready
+        ready_setup=1
         
 # I feel better having one of these
 def main():
+    global mainwin,reworkwin
     # a new app instance
     app = QApplication(sys.argv)
     mainwin = MainWindow()
-    rework = Rework()
+    reworkwin = Rework()
     mainwin.show()
-    rework.show()
+    # reworkwin.show()
     # without this, the script exits immediately.
     sys.exit(app.exec_())
+
+# Create new thread for sending data every second
+try:
+    valueThread=ValueThread()
+except Exception as e:
+    print ("Error: unable to start thread!")
+    print (str(e))
+# Start thread
+valueThread.start()
 
 # python bit to figure how who started This
 if __name__ == "__main__":
--- pyqt5/rework.py
+++ pyqt5/rework.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'D:\Qt\HMI_Lakban\rework.ui'
+# Form implementation generated from reading ui file 'D:/Qt/Qt5-HMI-test/rework.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.2
 #
@@ -14,19 +14,19 @@ class Ui_Form(object):
         Form.resize(604, 376)
         Form.setStyleSheet("background-color: rgb(0, 0, 255);")
         self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(230, 10, 171, 51))
+        self.label.setGeometry(QtCore.QRect(220, 10, 191, 51))
         self.label.setStyleSheet("color: rgb(255, 170, 0);\n"
-"font: 87 24pt \"Arial Black\";")
+"font: 75 28pt \"Arial\";")
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Form)
         self.label_2.setGeometry(QtCore.QRect(30, 110, 81, 31))
         self.label_2.setStyleSheet("color: rgb(255, 255, 255);\n"
-"font: 75 22pt \"MS Shell Dlg 2\";")
+"font: 75 20pt \"Arial\";")
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(Form)
         self.label_3.setGeometry(QtCore.QRect(30, 175, 221, 61))
         self.label_3.setStyleSheet("color: rgb(255, 255, 255);\n"
-"font: 75 22pt \"MS Shell Dlg 2\";")
+"font: 75 20pt \"Arial\";")
         self.label_3.setObjectName("label_3")
         self.line_val = QtWidgets.QLabel(Form)
         self.line_val.setGeometry(QtCore.QRect(340, 100, 161, 41))
@@ -37,44 +37,43 @@ class Ui_Form(object):
         self.line_val.setObjectName("line_val")
         self.pb_submit = QtWidgets.QPushButton(Form)
         self.pb_submit.setGeometry(QtCore.QRect(0, 280, 611, 101))
-        self.pb_submit.setStyleSheet("font: 75 26pt \"MS Shell Dlg 2\";\n"
+        self.pb_submit.setStyleSheet("font: 75 28pt \"Arial\";\n"
 "background-color: rgb(255, 170, 0);\n"
 "color: rgb(255, 255, 255);")
         self.pb_submit.setObjectName("pb_submit")
-        self.label_5 = QtWidgets.QLabel(Form)
-        self.label_5.setGeometry(QtCore.QRect(540, -10, 41, 71))
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.label_5.sizePolicy().hasHeightForWidth())
-        self.label_5.setSizePolicy(sizePolicy)
-        self.label_5.setStyleSheet("color: rgb(255, 0, 0);\n"
-"font: 75 48pt \"MS Shell Dlg 2\";")
-        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_5.setObjectName("label_5")
-        self.next_btn = QtWidgets.QLabel(Form)
-        self.next_btn.setGeometry(QtCore.QRect(476, 180, 94, 49))
-        self.next_btn.setStyleSheet("color: rgb(255, 255, 255);\n"
-"font: 75 22pt \"MS Shell Dlg 2\";")
-        self.next_btn.setText("")
-        self.next_btn.setPixmap(QtGui.QPixmap("right_chevron.png"))
-        self.next_btn.setScaledContents(False)
-        self.next_btn.setAlignment(QtCore.Qt.AlignCenter)
-        self.next_btn.setObjectName("next_btn")
-        self.prev_btn = QtWidgets.QLabel(Form)
-        self.prev_btn.setGeometry(QtCore.QRect(290, 180, 94, 49))
-        self.prev_btn.setStyleSheet("color: rgb(255, 255, 255);\n"
-"font: 75 22pt \"MS Shell Dlg 2\";")
-        self.prev_btn.setText("")
-        self.prev_btn.setPixmap(QtGui.QPixmap("left_chevron.png"))
-        self.prev_btn.setAlignment(QtCore.Qt.AlignCenter)
-        self.prev_btn.setObjectName("prev_btn")
-        self.rework_val = QtWidgets.QLabel(Form)
-        self.rework_val.setGeometry(QtCore.QRect(390, 180, 80, 49))
+        self.exit_pb = QtWidgets.QPushButton(Form)
+        self.exit_pb.setGeometry(QtCore.QRect(540, 10, 50, 41))
+        self.exit_pb.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgb(243, 0, 0);\n"
+"font: 75 28pt \"Arial\";")
+        self.exit_pb.setObjectName("exit_pb")
+        self.horizontalWidget = QtWidgets.QWidget(Form)
+        self.horizontalWidget.setGeometry(QtCore.QRect(300, 160, 244, 80))
+        self.horizontalWidget.setObjectName("horizontalWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setSpacing(10)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pb_prev = QtWidgets.QPushButton(self.horizontalWidget)
+        self.pb_prev.setMinimumSize(QtCore.QSize(0, 50))
+        self.pb_prev.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgb(255, 170, 0);\n"
+"font: 75 24pt \"Arial\";")
+        self.pb_prev.setObjectName("pb_prev")
+        self.horizontalLayout.addWidget(self.pb_prev)
+        self.rework_val = QtWidgets.QLabel(self.horizontalWidget)
         self.rework_val.setStyleSheet("color: rgb(255, 255, 255);\n"
-"font: 75 28pt \"MS Shell Dlg 2\";")
+"font: 75 24pt \"Arial\";")
         self.rework_val.setAlignment(QtCore.Qt.AlignCenter)
         self.rework_val.setObjectName("rework_val")
+        self.horizontalLayout.addWidget(self.rework_val, 0, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        self.pb_next = QtWidgets.QPushButton(self.horizontalWidget)
+        self.pb_next.setMinimumSize(QtCore.QSize(0, 50))
+        self.pb_next.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgb(255, 170, 0);\n"
+"font: 75 24pt \"Arial\";")
+        self.pb_next.setObjectName("pb_next")
+        self.horizontalLayout.addWidget(self.pb_next)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -87,6 +86,8 @@ class Ui_Form(object):
         self.label_3.setText(_translate("Form", "Jumlah Rework :"))
         self.line_val.setText(_translate("Form", "Line 23"))
         self.pb_submit.setText(_translate("Form", "SUBMIT"))
-        self.label_5.setText(_translate("Form", "x"))
+        self.exit_pb.setText(_translate("Form", "x"))
+        self.pb_prev.setText(_translate("Form", "<<"))
         self.rework_val.setText(_translate("Form", "1000"))
+        self.pb_next.setText(_translate("Form", ">>"))
 
