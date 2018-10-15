61aa289d1c59c8da4e65c31eef50d500bec3022c
mfa077 <miftahf77@gmail.com>
2018-10-15 15:59:08 +0700

    HMI Lakban using PyQt5

--- pyqt5/popup.py
+++ pyqt5/popup.py
@@ -11,7 +11,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(200, 50)
+        Form.resize(229, 50)
+        Form.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
         self.popup_text = QtWidgets.QLabel(Form)
         self.popup_text.setGeometry(QtCore.QRect(0, 0, 200, 50))
         self.popup_text.setStyleSheet("color: rgb(0, 79, 239);\n"
@@ -22,6 +23,12 @@ class Ui_Form(object):
         self.popup_text.setLineWidth(2)
         self.popup_text.setAlignment(QtCore.Qt.AlignCenter)
         self.popup_text.setObjectName("popup_text")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(199, 0, 30, 30))
+        self.pushButton.setStyleSheet("font: 16pt \"Arial\";\n"
+"background-color: rgb(255, 0, 0);\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton.setObjectName("pushButton")
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -30,4 +37,5 @@ class Ui_Form(object):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
         self.popup_text.setText(_translate("Form", "Rework has been submitted."))
+        self.pushButton.setText(_translate("Form", "x"))
 
